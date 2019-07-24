15719577104c6eed871472b00dc6df017c913ff0
Erick Laureano <erickllaureano@gmail.com>
2019-07-24 15:19:36 -0500

    se crean los modelos iniciales, para el control de usuarios, y carros

--- catalogues/models.py
+++ catalogues/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'Users'
+
+    def __str__(self):
+        return self.name
+
+
+class Cars(models.Model):
+    modelo = models.CharField(
+        max_length=50
+    )
+    marca = models.CharField(
+        max_length=50
+    )
+    color = models.CharField(
+        max_length=50
+    )
+    placa = models.CharField(
+        max_length=10,
+        unique=True
+    )
+
+    class Meta:
+        db_table = 'Cars'
+
+    def __str__(self):
+        return '' + str(self.modelo) + ' - ' + str(self.placa)
--- control/models.py
+++ control/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from catalogues import models as catalogModels
 
-# Create your models here.
+
+class AssignedCars(models.Model):
+    idCar = models.ForeignKey(
+        catalogModels.Cars,
+        on_delete=models.DO_NOTHING
+    )
+    idUser = models.ForeignKey(
+        catalogModels.Users,
+        on_delete=models.DO_NOTHING
+    )
+
+    class Meta:
+        unique_together = ('idCar', 'idUser')
+        db_table = 'AssignedCar'
+
+
+class UsedCar(models.Model):
+    expiration = models.DateTimeField()
+    idAssignedCar = models.OneToOneField(
+        AssignedCars,
+        on_delete=models.DO_NOTHING,
+        primary_key=True
+    )
+
+    class Meta:
+        db_table = 'UsedCar'
